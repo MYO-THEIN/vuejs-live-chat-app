cb049692c270b033605ef278aab7e2c9b2690a1f
MYO-THEIN <okmt21@gmail.com>
2022-11-25 21:14:55 +0630

    SignUp and Login components design

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+        <input type="text" placeholder="Email" v-model="email" />
+        <input type="text" placeholder="Password" v-model="password" />
+        <button>Login</button>
+    </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        // data
+        let email = ref(""),
+            password = ref("");
+
+        // methods
+        let login = ()=>{
+
+        }
+
+        return {
+            email, password,
+            login
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>Sign Up</h2>
+    <form @submit.prevent="signUp">
+        <input type="text" placeholder="User name" v-model="username" />
+        <input type="text" placeholder="Email" v-model="email" />
+        <input type="text" placeholder="Password" v-model="password" />
+        <button>Sign Up</button>
+    </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        // data
+        let username = ref(""),
+            email = ref(""),
+            password = ref("");
+
+        // methods
+        let signUp = ()=>{
+
+        }
+
+        return {
+            username, email, password,
+            signUp
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
